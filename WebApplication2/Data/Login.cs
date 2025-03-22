1795a40acc956d76d40c98f13188af531c14d2a5
majdyaq <majd@feelit.tech>
2025-03-22 16:05:54 +0200

    Add project files.

---
+++ WebApplication2/Data/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Data
+{
+    public class Login
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ WebApplication2/Data/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Data
+{
+    public class UserRole
+    {
+        public string Role { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+    }
+}
